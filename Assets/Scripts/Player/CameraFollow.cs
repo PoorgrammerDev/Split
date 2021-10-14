9365701931c301d6c9b4ee04acee0fc083905123
PoorgrammerDev <58582458+PoorgrammerDev@users.noreply.github.com>
2021-10-14 08:56:15 -0700

    Added Camera Smooth Follow

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Split {
+
+    [RequireComponent(typeof(Camera))]
+    public class CameraFollow : MonoBehaviour {
+        [SerializeField] private Transform target;
+        
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float smoothSpeed;
+
+        [SerializeField] private Vector3 offset;
+
+        private Vector3 velocity = Vector3.zero;
+
+        //Makes camera follow target smoothly
+        void LateUpdate() {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.SmoothDamp(this.transform.position, desiredPosition, ref this.velocity, this.smoothSpeed);
+
+            this.transform.position = smoothedPosition;
+        }
+
+
+
+    }
+}
+
+
