1bdf9cc5e8e05aa07c81f6a54e6f973e6090b63b
Poorgrammer <58582458+PoorgrammerDev@users.noreply.github.com>
2022-03-26 00:19:29 -0700

    small bit of work on main menu
    committing from laptop to work on desktop

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -3,5 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour {
-    
+   
+
+
+	public void PlayClick() {
+		
+	}
+
+	public void BuilderClick() {
+		
+	}
+
+	public void SettingsClick() {
+		
+	}
+	
+	public void TutorialClick() {
+		
+	}
+ 
+	public void AboutClick() {
+		
+	}
+	
+	public void ExitClick() {
+		Application.Quit();
+	}
+ 
 }
